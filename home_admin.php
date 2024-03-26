f97f7a89ef75ba5eaaba6a21ec8c70937790c5fb
aulfeb <93027849+aulfeb@users.noreply.github.com>
2024-03-26 19:27:03 +0700

    Update home_admin.php

--- home_admin.php
+++ home_admin.php
@@ -9,10 +9,25 @@
 </head>
 
 <body>
+    <?php
+    // Panggil file koneksi_db.php
+    include "koneksi_db.php";
 
+    $query = "SELECT * FROM post";
+    // Eksekusi query
+    $result = mysqli_query($koneksi, $query);
+    ?>
     <header>
         <div class="container">
             <h6 class="display-6 text-black text-center">Halaman Admin - Artikel</h6>
+            <ul class="nav nav-pills">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Daftar Artikel</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Daftar AKun</a>
+                </li>
+            </ul>
         </div>
     </header>
 
@@ -20,8 +35,13 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-12 mx-auto">
-                    <div class="float-md-end">
-                        <button type="button" class="btn btn-success">Tambah Artikel</button>
+                    <div class="d-flex justify-content-end">
+                        <a href="tambah_artikel.php">
+                            <button type="button" class="btn btn-success">Tambah Artikel</button>
+                        </a>
+                        <a href="logout.php">
+                            <button type="button" class="btn btn-danger mx-4">Keluar</button>
+                        </a>
                     </div>
                 </div>
             </div>
@@ -34,8 +54,28 @@
                         <th scope="col">Kontent</th>
                         <th scope="col">Tanggal Dipost</th>
                         <th scope="col">Nama Penulis</th>
+                        <th scope="col">Actions</th>
                     </tr>
                 </thead>
+                <tbody>
+                    <?php
+                    $no = 1; // Inisialisasi nomor urut
+                    while ($row = mysqli_fetch_array($result)) {
+                        // Menampilkan data dalam baris tabel
+                        echo "<tr>";
+                        echo "<td>" . $no++ . "</td>";
+                        echo "<td>" . $row['title'] . "</td>";
+                        echo "<td>" . $row['content'] . "</td>";
+                        echo "<td>" . $row['date'] . "</td>";
+                        echo "<td>" . $row['username'] . "</td>";
+                        echo "<td>
+                                        <button type='button' class='btn btn-primary'>Edit</button>
+                                        <button type='button' class='btn btn-danger'>Hapus</button>
+                                      </td>";
+                        echo "</tr>";
+                    }
+                    ?>
+                </tbody>
             </table>
         </div>
     </section>
